b628850cf2fdb5e30f7c8e4616e378a241588d36
Edgard M. Marquez Aylas <edgardma@gmail.com>
2020-09-30 23:32:48 -0500

    Imprimir las variables en consola

--- CursoJS/01-fundamentos/app.js
+++ CursoJS/01-fundamentos/app.js
@@ -1,1 +1,22 @@
-console.log('Hola Mundo');
+// console.log('Hola Mundo');
+
+let a = 10, 
+    b = 20, 
+    c = 'Hola ', 
+    d = 'Edgard', 
+    x = a + b;
+
+const saludo = c + d;
+
+console.log('a', a);
+console.warn('b', b);
+console.error('c', c);
+console.info('d', d);
+
+console.log('%c Mis variables', 'color:red; font-weight: bold');
+console.log({ a });
+console.warn({ b });
+console.error({ c });
+console.info({ d });
+
+console.table({a, b, c, d, x});
