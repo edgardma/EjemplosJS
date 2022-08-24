a7695cfa7c432ea9a46bd6334944c64f266def62
Edgard M. Marquez Aylas <edgardma@gmail.com>
2022-08-24 10:36:21 -0500

    Proyecto inicial de Challenge

---
+++ hand-challenge/index.js
@@ -0,0 +1,22 @@
+/**
+ * 
+
+point_right : moves the memory pointer to the next cell
+
+point_left : moves the memory pointer to the previous cell
+
+point_up_2 : increment the memory cell at the current position
+
+point_down : decreases the memory cell at the current position.
+
+fist_right : if the memory cell at the current position is 0, jump just after the corresponding fist_left
+
+fist_left : if the memory cell at the current position is not 0, jump just after the corresponding fist_right
+
+fist_oncoming : Display the current character represented by the ASCII code defined by the current position.
+
+ */
+
+const MIN_CELL = 0
+const MAX_CELL = 255
+
